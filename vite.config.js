73bf142b64b3bcdefe7b2d9c16dea2f01eb2fbbf
Emmanuel Miranda <65192290+emiranda028@users.noreply.github.com>
2026-07-08 12:09:45 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Si vas a publicar en GitHub Pages en un repo que NO es <usuario>.github.io,
-// descomentá la línea de "base" y poné el nombre exacto de tu repositorio.
-export default defineConfig({
-  plugins: [react()],
-  base: "/monitoreo_transporte_pba/",
-});
